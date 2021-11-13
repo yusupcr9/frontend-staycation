0aa3f9dd0b9e8763a40e2b13bfdecbb748bb4e47
Yusup Firmansyah <yhusup6@gmail.com>
2021-11-13 21:47:59 +0700

    Component Hero (TODO: styling hero (button))

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import Header from "parts/Header";
-
+import Hero from "parts/Hero";
+import landingPage from "json/landingPage.json";
 export default class LandingPage extends Component {
   render() {
     return (
       <React.Fragment>
         <Header {...this.props}></Header>
+        <Hero data={landingPage.hero} />
       </React.Fragment>
     );
   }
