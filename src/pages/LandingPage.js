6ad94e47d7f3820b6f760c0ee6723303999053a9
Yusup Firmansyah <yhusup6@gmail.com>
2021-11-14 23:13:53 +0700

    Complete Section Testimonials

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import Header from "parts/Header";
 import Hero from "parts/Hero";
 import MostPicked from "parts/MostPicked";
+import Categories from "parts/Categories";
+import Testimony from "parts/Testimony";
 
 import landingPage from "json/landingPage.json";
 
@@ -19,6 +21,8 @@ export default class LandingPage extends Component {
           refMostPicked={this.refMostPicked}
           data={landingPage.mostPicked}
         />
+        <Categories data={landingPage.categories} />
+        <Testimony data={landingPage.testimonial} />
       </React.Fragment>
     );
   }
